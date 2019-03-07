34dffacacabae3d6dd5f670280594281e2397426
natsumi-m7 <n.osada0724@gmail.com>
2019-03-07 17:22:35 +0900

    ヘッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  get 'seasons/show'
   root 'spots#top'
   devise_for :users
   get '/', to: 'spots#top'
-  get '/season', to: 'spots#season'
   resources :users
-  resources :spots do
-    resources :comments,only:[:create,:destroy]
-    resource :favorites,only:[:create,:destroy]
-    resources :went_spots,only:[:create,:destroy]
+  resources :seasons,only:[:show] do
+    resources :spots do
+      resources :comments,only:[:create,:destroy]
+      resource :favorites,only:[:create,:destroy]
+      resources :went_spots,only:[:create,:destroy]
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
