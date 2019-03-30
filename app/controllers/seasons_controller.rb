9c5c3924e76ea00ff4ab81a108561209e06937d5
nastumi-m7 <n.osada0724@gmail.com>
2019-03-30 10:50:47 +0000

    seasonコントローラの修正

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,7 +1,7 @@
 class SeasonsController < ApplicationController
   def spring
   	@search = Spot.ransack(params[:q])
-  	@spots = @search.result.where(season: "春")
+  	@spots = @search.result.where(season: "春").order(created_at: :desc)
   	# .where(season: "春")
     # @q = Spot.where(season: "春").ransack(params[:q])
     # @spots = @q.result(distinct: true)
@@ -9,18 +9,18 @@ class SeasonsController < ApplicationController
 
   def summer
     @search = Spot.ransack(params[:q])
-    @spots = @search.result.where(season: "夏")
+    @spots = @search.result.where(season: "夏").order(created_at: :desc)
   end
 
   def autumn
     @search = Spot.ransack(params[:q])
-    @spots = @search.result.where(season: "秋")
+    @spots = @search.result.where(season: "秋").order(created_at: :desc)
   end
 
 
   def winter
     @search = Spot.ransack(params[:q])
-    @spots = @search.result.where(season: "冬")
+    @spots = @search.result.where(season: "冬").order(created_at: :desc)
   end
 
 
