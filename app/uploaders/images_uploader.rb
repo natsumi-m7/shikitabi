da300d1d0094dbb945186729ebdd0ca5d6269e20
natsumi-m7 <n.osada0724@gmail.com>
2019-03-31 18:02:26 +0900

    noimage画像の編集

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -18,7 +18,7 @@ class ImagesUploader < CarrierWave::Uploader::Base
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-    "/images/no_image.png/"
+    "no_image.png"
   end
 
   # Process files as they are uploaded:
